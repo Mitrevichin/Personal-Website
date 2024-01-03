6b6d90b0feb031dc8c0de8cf10959eecb9a9089e
Petar Mitrevichin <pmitrevichin@gmail.com>
2024-01-03 17:19:15 +0200

    about page started

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+
+
+                                    const person = {
+                                        firstName: "Petar",
+                                        lastName: "Mitrevichin",
+                                        profession: "Web Developer",
+                                        education: {
+                                            degree: "Bachelor's in Business Administration",
+                                            university: "South-West University",
+                                            graduationYear: 2022
+                                        },
+                                        languages: ["English", "Bulgarian"],
+                                        hobby: "Styling buttons"
+                                    }
